abbed480624b64a5fcb1f1d9316242cc5be5c50f
Hantao Zhong <moon991015@gmail.com>
2019-10-05 15:06:15 +0100

    Add voltage funtion.

--- bms.cpp
+++ bms.cpp
@@ -5,8 +5,12 @@ BatteryManagementSystem::BatteryManagementSystem()
 }
 
 /// TASK 9 (BOTH) //////////////////////////////////////////////////////////////
+
 double BatteryManagementSystem::get_current()
 {
   return 5;
+double BatteryManagementSystem::get_voltage()
+{
+    return 0;
 }
 /// END OF TASKS 9AB ///////////////////////////////////////////////////////////
